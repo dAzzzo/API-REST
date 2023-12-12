f8725a7f7cd734eb17e7139326ce06ea683de9aa
Daniel <dandcom0601@g.educaand.es>
2023-12-12 13:21:40 +0100

    guardado 12 dic 23

--- indexExamen.js
+++ indexExamen.js
@@ -18,7 +18,7 @@ const express = require("express");
 const helmet = require("helmet");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swaggerExamen.json");
-const url = "mongodb://localhost:27017/greidsla";
+const url = "mongodb://127.0.0.1:27017";
 const client = new MongoClient(url);
 
 // Database Name
@@ -31,7 +31,6 @@ let findResult = null;
 async function main() {
   // Use connect method to connect to the server
   await client.connect();
-  console.log("Arriba Perú csmr!!!");
 
   db = client.db(greidsla);
   collection = db.collection("greidsla");
